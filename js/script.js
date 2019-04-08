3f6ee86e9740199868f2a979e8b240fe9ce7cb7b
Agata Stawarz <agata.stawarz94@gmail.com>
2019-04-08 22:40:23 +0200

    Add radio card toggle

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-new Vue({
+Vue.component('card', {
+    template: '#card',
+    methods: {
+      toggle() {
+       this.showSection = !this.showSection
+      }
+    },
+    data() {
+      return {
+        showSection: false,
+        imageUrl: 'https://images.unsplash.com/photo-1547679905-3c132ce6d489?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg',
+      }
+    }
+  })
+  
+var app = new Vue({
     el: '#vue-app',
     data: {
         stations: [
@@ -14,5 +29,9 @@ new Vue({
     methods: {
 
     },
+
+    computed: {
+       
+    }
 });
 
