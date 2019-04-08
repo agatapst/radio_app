d4d4a7e335a6ff0f152757fb630be480fd564f49
Agata Stawarz <agata.stawarz94@gmail.com>
2019-04-08 20:37:36 +0200

    Add initial project structure

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+function compileScss() {
+    return (
+        gulp
+            .src("scss/main.scss")
+            .pipe(sass())
+            .on("error", sass.logError)
+            .pipe(gulp.dest("dist/css"))
+    );
+}
+
+function watch() {
+    compileScss();
+    gulp.watch('scss/*.scss', compileScss)
+}
+
+exports.watch = watch;
---
+++ js/script.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        
+    },
+    
+    methods: {
+
+    },
+});
+
+
+// function init(){
+// }
+
+// document.addEventListener('DOMContentLoaded', init);
