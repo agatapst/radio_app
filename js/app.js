48558eca4d286dc1584be12278b11ba4ac35d640
Agata Stawarz <agata.stawarz94@gmail.com>
2019-04-09 11:10:22 +0200

    Add feature to toggle only one radio station

---
+++ js/app.js
@@ -0,0 +1,38 @@
+Vue.component('card', {
+    template: '#card',
+    props: {
+        radioStation: Object,
+        showSection: Boolean,
+        onTitleClick: Function
+    },
+    methods: {},
+    data() {
+      return {
+        imageUrl: 'https://images.unsplash.com/photo-1547679905-3c132ce6d489?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg',
+      }
+    }
+  })
+  
+var app = new Vue({
+    el: '#vue-app',
+    data: {
+        stations: [
+            {name: 'Radio ZET', fm: 81.5},
+            {name: 'RMF FM', fm: 101.2},
+            {name: 'Eska Rock', fm: 106.6},
+            {name: 'Chili Zet', fm: 105.1},
+            {name: 'Classic', fm: 88.1}
+       ],
+       currentStationName: null
+    },
+    methods: {
+        chooseStation: function (radioStation) {
+          // set current station name as the one clicked
+            this.currentStationName = radioStation.name;
+        }
+    },
+    computed: {
+       
+    }
+});
+
